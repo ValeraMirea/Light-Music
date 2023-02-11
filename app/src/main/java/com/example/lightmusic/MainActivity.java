17ecb80ca3a15eb775c548e4ac108a36afd966eb
Valerii <teslav77@gmail.com>
2023-02-11 21:35:43 +0300

    First Update LightMusic

--- app/src/main/java/com/example/lightmusic/MainActivity.java
+++ app/src/main/java/com/example/lightmusic/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.lightmusic;
 
+import static android.app.PendingIntent.getActivity;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,27 +30,35 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.bluetooth.BluetoothSocket;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
 
     //Переменные
+
     private int ChooseMode;
     private int counter;
+    private final static int REQUEST_ENABLE_BT = 0;
+
+    //Управляющие элементы
+
     private EditText modeField;
     private Button power_btn;
     private Button change_btn;
     private ImageView bl_Image;
-    private boolean Bluetooth_flag;
 
     // Bluetooth
+
     private BluetoothAdapter myBluetoothAdapter;
-    // private BluetoothClient mBluetoothClient;
-    private BluetoothDevice mBluetoothDevice;
-    private BluetoothSocket mBluetoothSocket;
 
     public MainActivity() {
     }
-
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
         ChooseMode = 1;
         counter = 1;
-        Bluetooth_flag = false;
 
         //Создание и инициализация компонентов и блоков программы
 
@@ -60,6 +74,9 @@ public class MainActivity extends AppCompatActivity {
         final Button change_btn = findViewById(R.id.ChooseButton);
         final TextView modeField = findViewById(R.id.TextModeWorking);
         final ImageView bl_Image = findViewById(R.id.Bluetooth);
+
+        // Инициализация Bluetooth
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
         // еще вариант инициализации переменых для работы программы
@@ -71,99 +88,147 @@ public class MainActivity extends AppCompatActivity {
 
         // Отключаем кнопку переключения режима по умолчанию
         change_btn.setEnabled(false);
-        bl_Image.setImageResource(R.drawable.ic_action_off);
+        if (Check_Bluetooth()){
+            bl_Image.setImageResource(R.drawable.ic_action_on);
+            power_btn.setEnabled(true);
+        }
+        else {
+            bl_Image.setImageResource(R.drawable.ic_action_off);
+            power_btn.setEnabled(false);
+        }
 
-        // Инициализация Bluetooth
-        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        // mBluetoothClient= new BluetoothClient(myBluetoothAdapter);
-        // mMode = "Mode1";
+        // Обработка нажатия кнопки питания для адресной ленты
+        power_btn.setOnClickListener(view -> {
 
-
-//        // Обработчик событий Bluetooth
-//        mBluetoothClient.setHandler(new Handler(){
-//            // Обработчик сообщений
-//            @Override
-//            public void handleMessage(Message msg) {
-//
-//                if (msg.what == BluetoothClient.MESSAGE_READ) {
-//                    String readMessage = null;
-//                    try {
-//                        readMessage = new String((byte[]) msg.obj, "UTF-8");
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//
-//                    // Обновить UI поле с текущим режимом
-//                    modeField.setText(readMessage);
-//
-//                    // Вывести сообщение Toast о успешной настройке режима
-//                    Toast.makeText(getApplicationContext(), "Mode configured successfully.",
-//                            Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
-
-        // Обработка нажатия кнопки питания
-        power_btn.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            public void onClick(View view) {
-                if (myBluetoothAdapter == null) {
-                    // Устройство не поддерживает Bluetooth
-                    Toast.makeText(getApplicationContext(), "Это устройство не поддерживает Bluetooth",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    if (!myBluetoothAdapter.isEnabled()) {
-                        // Включаем Bluetooth
-                        //myBluetoothAdapter.enable();
-                        Toast.makeText(getApplicationContext(), "Модуль Bluetooth отключен",
-                                Toast.LENGTH_LONG).show();
-                        bl_Image.setImageResource(R.drawable.ic_action_off);
-                        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        Bluetooth_flag = false;
-
-                    }
-                    else {
-                        // Включаем кнопку переключения режима
-                        Bluetooth_flag = true;
-                        if (counter==1){
-                            power_btn.setText("Power ON");
-                            change_btn.setEnabled(true);
-                            bl_Image.setImageResource(R.drawable.ic_action_on);
-                            counter+=1;
-                        }
-                        else if (counter!=1){
-                            power_btn.setText("Power OFF");
-                            change_btn.setEnabled(false);
-                            bl_Image.setImageResource(R.drawable.ic_action_off);
-                            counter -= 1;
-                        }
-
-                    }
-                }
+            // Включаем кнопку переключения режима
+            if (counter == 1) {
+                power_btn.setText("Power ON");
+                change_btn.setEnabled(true);
+                //bl_Image.setImageResource(R.drawable.ic_action_on);
+                counter += 1;
+            } else {
+                power_btn.setText("Power OFF");
+                change_btn.setEnabled(false);
+                //bl_Image.setImageResource(R.drawable.ic_action_off);
+                counter -= 1;
             }
+
         });
 
         //обработчик событий кнопки переключения режимов
 
-        change_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (ChooseMode++){
-                    case 1:
-                        modeField.setText("Выбран режим столбик громкости от зеленого к красному");
-                        break;
-                    case 2:
-                        modeField.setText("Выбран режим столбик громкости бегущая радуга");
-                        break;
-                    case 3:
-                        modeField.setText("Выбран режим светомузыки по частотам (5 полос симетрично)");
-                        break;
-                    case 4:
-                        modeField.setText("Выбран режим светомузыки по частотам (3 полосы) ");
-                        ChooseMode = 0;
-                        break;
-                }
+        change_btn.setOnClickListener(view -> {
+            switch (ChooseMode++) {
+                case 1:
+                    modeField.setText("Выбран режим столбик громкости от зеленого к красному");
+                    break;
+                case 2:
+                    modeField.setText("Выбран режим столбик громкости бегущая радуга");
+                    break;
+                case 3:
+                    modeField.setText("Выбран режим светомузыки по частотам (5 полос симетрично)");
+                    break;
+                case 4:
+                    modeField.setText("Выбран режим светомузыки по частотам (3 полосы) ");
+                    ChooseMode = 0;
+                    break;
             }
         });
     }
+    private boolean Check_Bluetooth() {
+        boolean bluetooth_flag;
+        if (myBluetoothAdapter == null) {
+                    // Устройство не поддерживает Bluetooth
+                    Toast.makeText(getApplicationContext(), "Это устройство не поддерживает Bluetooth",
+                            Toast.LENGTH_LONG).show();
+                    bluetooth_flag = false;
+                } else if (!myBluetoothAdapter.isEnabled()) {
+                    Toast.makeText(getApplicationContext(), "Модуль Bluetooth отключен",
+                            Toast.LENGTH_LONG).show();
+                    bluetooth_flag = false;
+                } else {
+                    Toast.makeText(getApplicationContext(), "Модуль Bluetooth включен",
+                            Toast.LENGTH_LONG).show();
+                    bluetooth_flag = true;
+                }
+        return bluetooth_flag;
+    }
+
+
+//    public void Bluetooth(){
+//        if (!myBluetoothAdapter.isEnabled()) {
+//            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
+//            {
+//                Toast.makeText(getApplicationContext(), "Модуль Bluetooth отключен",
+//                        Toast.LENGTH_LONG).show();
+//                bl_Image.setImageResource(R.drawable.ic_action_off);
+//            }
+//            startActivity(turnOn);
+//        } else {
+//            Toast.makeText(getApplicationContext(), "Модуль Bluetooth включен",
+//                        Toast.LENGTH_LONG).show();
+//                bl_Image.setImageResource(R.drawable.ic_action_on);
+//        }
+//        Toast.makeText(getApplicationContext(), "Это устройство не поддерживает Bluetooth",
+//                        Toast.LENGTH_LONG).show();
+//                bl_Image.setImageResource(R.drawable.ic_action_off);
+//                myBluetoothAdapter.disable();
+//    }
+//    public boolean BluetoothON() {
+//        if (!myBluetoothAdapter.isEnabled()) {
+//            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
+//            {
+//                return false;
+//            }
+//            startActivity(turnOn);
+//        } else {
+//            return true;
+//        }
+//        return false;
+//    }
+//    public void GetPairDevise(ArrayList<String> list, BluetoothAdapter bluetoothAdapter) {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+//            // Выдать сообщение о ошибке
+//            return;
+//        }
+//        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+//        if (pairedDevices.size() > 0) {
+//            for (BluetoothDevice device : pairedDevices) {
+//                arrayNewDevice.add(device);
+//                list.add(device.getName() + "  -  " + device.getAddress());
+//            }
+//        }
+//    }
+//    public void GetScanDevise(BluetoothAdapter mYBluetoothAdapter) {
+//        if (listNewDeviceAdapter.getCount() != 0)
+//            listNewDeviceAdapter.clear();
+//        arrayNewDevice.clear();
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        mYBluetoothAdapter.startDiscovery();
+//        IntentFilter myFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+//        registerReceiver(mResiver, myFilter);
+//        myFilter = new IntentFilter((BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+//        registerReceiver(mResiver, myFilter);
+//    }
+//    private final BroadcastReceiver mResiver = new BroadcastReceiver() {
+//        @SuppressLint("MissingPermission")
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            String action = intent.getAction();
+//            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                myDevice =  intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                if (myDevice.getName() != null) {
+//                    arrayNewDevice.add(myDevice);
+//                    deviseScanList.add(myDevice.getName() + "  -  " + myDevice.getAddress());
+//                    listDevice.setAdapter(listNewDeviceAdapter);
+//                }
+//            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+//                myBluetoothAdapter.cancelDiscovery();
+//            }
+//        }
+//    };
 }
